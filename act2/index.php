3684bec763b10cb41af71381c4d8bf6d638ad213
cejay <cjgalos1983@gmail.com>
2026-08-30 22:45:57 +0000

    c

---
+++ act2/index.php
@@ -0,0 +1,13 @@
+<?php include 'layout/header.php'; ?>
+
+<section class="home">
+    <h1>Welcome to My Website</h1>
+    <p>This is the starting page of our simple PHP website.</p>
+
+    <div class="buttons">
+        <a href="register.php">Create an Account</a>
+        <a href="login.php">Login</a>
+    </div>
+</section>
+
+<?php include 'layout/footer.php'; ?>
